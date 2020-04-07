5f83e00558ec435145dbe3fd9b5cef6938ccbb93
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2020-04-07 16:23:20 -0400

    Update botapi.php

--- botapi.php
+++ botapi.php
@@ -1,6 +1,16 @@
 <?php
-if (isset($_POST["user"]) and isset($_POST["token"])) {
-	if (isset($_POST["minitwow"])) {
+function psrand($rspnum,$screennum) {
+	$lrseed = (int)$rspnum*(int)$screennum+(13**5);
+	$m = 2147483647;
+	$a = 16807;
+	$fseed = floor((((round(fmod(((float)$lrseed*exp(1)),1)*$m*$a))%$m)/$m)*(10**7))/(10**7);
+	return((round($fseed*$m*$a))%$m);
+}
+function cmp($a,$b) {
+	return($a[2]-$b[2]);
+}
+if (isset($_POST["minitwow"]) and (isset($_POST["screenname"]))) {
+	if (preg_match("/(MS)|([A-Z]{4})/", $_POST["screenname"])) {
 		$minitwowname = $_POST["minitwow"];
 		$configs = include("./private/config.php");
 		$servername = $configs["servername"];
@@ -12,111 +22,111 @@ if (isset($_POST["user"]) and isset($_POST["token"])) {
 			// set the PDO error mode to exception
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch(PDOException $e) {    
-			echo "Database connection failed. Please reload and contact.";
+			echo(json_encode(["failure","Database connection failed. Please reload and contact."]));
+			ob_end_flush();
 			die(111);
 			}
-		$sql = "SELECT mode, contestantsdata, google_sheets_id, deadline FROM minitwowinfo WHERE uniquename = ?";
+		$sql = "SELECT mode, description, contestantsdata, discord_link FROM minitwowinfo WHERE uniquename = ?";
 		$stmt = $conn->prepare($sql);
 		$stmt->execute([$minitwowname]);
 		$data = [];
 		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$data = $row;
 		}
-		require_once $configs["googleapifilev2"];
-		require_once "./googleapi/google-api-php-client-2.2.3/vendor/google/apiclient-services/src/Google/Service/Sheets.php";
-		$client = new \Google_Client();
-		if ($_POST["sender"] == 512593919230476298) {
-			$sql2 = "SELECT api_master_key FROM user_verify_table WHERE discord_id = ?";
-			$stmt2 = $conn->prepare($sql2); 
-			$stmt2->execute([$_POST["sender"]]);
-			$data2 = [];
-			while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
-				$data2 = $row2;
-			}
-			if (hash_equals($data2["api_master_key"],$_POST["token"])) {
-				$contestantsdata = json_decode($data["contestantsdata"],TRUE);
-				$service = new \Google_Service_Sheets($client);
-				$client->setApplicationName('online-twow-voting');
-				$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-				$client->setAccessType('offline');
-				$client->setAuthConfig($configs["googlecredentials"]);
-				if ($data["mode"] == $_POST["mode"]) {
-					if (($data["deadline"] >= time()) or ($_POST["deadlinebypass"] == 1)) {
-						if ($data["mode"] == "respond") {
-							if (in_array($_POST["user"],$contestantsdata["contestants"])) {
-							} else {
-								echo (json_encode(["failure","You are not a contestant in this minitwow."]));
-								die(126);
-							}
-							$response = $_POST["response"];
-							array_push($contestantsdata["responses"],[$_POST["user"],$response]);
-							$spreadsheetId = $data["google_sheets_id"];
-							$range = "Responses!A1:C1";
-							$valueRange = new Google_Service_Sheets_ValueRange();
-							$valueRange->setValues(["values" => [$_POST["user"],$_POST["username"],time(),$response]]);
-							$conf = ["valueInputOption" => "RAW"];
-							$service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $conf);
-							$update = "UPDATE minitwowinfo SET contestantsdata = ? WHERE uniquename = ?";
-							$conn->prepare($update)->execute([json_encode($contestantsdata),$minitwowname]);
-							echo (json_encode(["success","Your response: ".htmlspecialchars($response).", has been sent to: ".htmlspecialchars($minitwowname)."\n Please note the policy for dealing with repetitive submissions in each minitwow is different, your latest response may count as an edit, or a DRP, please DM the host with more info if there may be any ambiguity."]));
-						} elseif ($data["mode"] == "vote") {
-							$spreadsheetId = $data["google_sheets_id"];
-							$range = "Voting!A1:D1";
-							$conf = ["valueInputOption" => "RAW"];
-							$votestr = $_POST["votes"];
-							$votersp = "";
-							$votes = explode(",", $votestr);
-							for($i = 0; $i < count($votes); $i++) {
-								$tvote = $votes[$i];
-								if ($tvote[0] == "[") {
-									$tvote = substr($tvote,1);
-								}
-								if (substr($tvote,-1) == "]") {
-									$tvote = substr($tvote,0,-1);
-								}
-								if (preg_match("/(((MEGA)\ [!-~]+)|([A-Z]{5}\ [A-Z]+))/",$tvote)) {
-									$tvote = (explode(" ",$tvote,2))[1];
-									if(strlen(count_chars($tvote,3)) == strlen($tvote)) { //good vote
-									} else {
-										if ($votersp == "") {
-											$votersp = "Something in your vote may be wrong, here is a list of votes that may have issues, these votes were still sent:\n";
-										}
-										$votersp = $votersp.$votes[$i]." -Failed unique character check, check for duplicate characters \n";
-									}
-								} else {
-									if ($votersp == "") {
-										$votersp = "Something in your vote may be wrong, here is a list of votes that may have issues, these votes were still sent:\n";
-									}
-									$votersp = $votersp.$votes[$i]." -Failed regex syntax check \n";
-								}
-								$valueRange = new Google_Service_Sheets_ValueRange();
-								$valueRange->setValues(["values" => [$_POST["user"],$_POST["username"],time(),$votes[$i]]]);
-								$service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $conf);
-								array_push($contestantsdata["votes"],[$_POST["user"],$votes[$i]]);
-							}
-							$update = "UPDATE minitwowinfo SET contestantsdata = ? WHERE uniquename = ?";
-							$conn->prepare($update)->execute([json_encode($contestantsdata),$minitwowname]);
-							if ($votersp == "") {
-								$votersp = "All your votes have been checked using an automatic script, finding no issues. Note that this script does not check for missing characters, and therefore cannot guarantee 100% that your vote is valid.";
-							}
-							echo (json_encode(["success","Votes: ".htmlspecialchars($votestr)." sent to ".htmlspecialchars($minitwowname)."\n".$votersp]));
-						}
-					} else {
-						echo(json_encode(["failure","Deadline past."]));
-						die(194);
+		if ($data["mode"] == "vote") {
+			$contestantsdata = json_decode($data["contestantsdata"],TRUE);
+			if ($contestantsdata["screenmode"] == "t") {
+				$output = "";
+				for ($i = 0; $i < count($contestantsdata["votingscreens"]); $i++) {
+					$output = $output.$contestantsdata["votingscreens"][$i];
+				}
+				echo(json_encode(["success","t",0,$output]));
+				die(181);
+			} elseif ($contestantsdata["screenmode"] == "n") {
+				if (isset($_POST["screenname"])) {
+					//DO STUFF
+					$formatted = TRUE;
+					$responses = $contestantsdata["responses"];
+					$screenarr = str_split($_POST["screenname"]);
+					$screenid = 0;
+					for ($i = 0; $i < count($screenarr); $i++) {
+						$screenid = $screenid + ((ord($screenarr[$i])-65)*(26**$i));
 					}
-				} else {
-					echo(json_encode(["failure","Mode given does not match current mode."]));
-					die(195);
+					$mi = 1;
+					for ($i = 0; $i < count($responses); $i++) {
+						if(count($responses[$i])==3) {
+							$responses[$i][2] = psrand($responses[$i][2],$screenid);
+						} else {
+							echo(json_encode(["failure","Some responses may not be in the new response format which contains the data needed for screen generation, please contact Random."]));
+							die(191);
+						}
+					}
+					if ($formatted == TRUE) {
+						//DO MORE STUFF
+						usort($responses,"cmp");
+						$output = "";
+						if ($_POST["screenname"] !== "MS") {
+							$sl = $contestantsdata["screenlength"];
+							$rspiter = floor((count($responses)-1)/$sl);
+							$rspr = 0;
+							if (count($responses) > $sl*26) {
+								$rspiter = 25;
+								$mi = 2;
+								$output = $output."WARNING: There are too many responses for all responses to be included on screens to be generated using alphabetical characters based on the settings given, please contact Random if you want to vote on all of them. (As if this will ever be seen by anyone).";
+							}
+							if (count($responses)%$sl == 0) {
+								$rspr = $sl;
+							} else {
+								$rspr = count($responses)%$sl;
+							}
+							for ($i = 0; $i < $rspiter; $i++) {
+								$output = $output."Screen ".htmlspecialchars($_POST["screenname"]).htmlspecialchars(chr($i+65));
+								for ($j = 0; $j < $sl; $j++) {
+									$output = $output."\n";
+									$output = $output.chr($j+65).": ";
+									$output = $output.($responses[$i*$sl+$j][1]);
+								}
+								$output = $output."\n\n";
+							}
+							$output = $output."Screen ".htmlspecialchars($_POST["screenname"]).htmlspecialchars(chr($rspiter+65));
+							for ($i = 0; $i < $rspr; $i++) {
+								$output = $output."\n";
+								$output = $output.(chr($i+65).": ");
+								$output = $output.($responses[$rspiter*$sl+$i][1]);
+							}
+						} else {
+							$output = $output.("Screen MEGA (Megascreen)");
+							$rspr = 0;
+							if (count($responses) > 94) {
+								$rspr = 94;
+								$output = $output."\n Note: As there are more than 94 responses, the megascreen does not contain all responses. Please contact Random if there are somehow more than 94 responses.";
+							} else {
+								$rspr = count($responses);
+							}
+							for ($i = 0; $i < $rspr; $i++) {
+								$output = $output.("\n");
+								$output = $output.(chr($i+33).": ");
+								$output = $output.(htmlspecialchars($responses[$i][1]));
+							}
+						}
+						echo(json_encode(["success","n",$mi,$output]));
+						die(182);
+					}
 				}
 			} else {
-				echo (json_encode(["failure","Bot backend verification error. Tell Random if this happens."]));
-				die(191);
+				echo(json_encode(["failure","The screenmode of the minitwow is not recognised, please contact Random."]));
+				die(192);
 			}
 		} else {
-			echo(json_encode(["failure","User does not have API setup."]));
+			echo(json_encode(["failure","This minitwow is not open for voting."]));
 			die(193);
 		}
+	} else { 
+		echo(json_encode(["failure","Your screen name does not satisfy the regex, your screenname has to be 4 capital English alphabet letters."]));
+		die(194);
 	}
+} else { 
+	echo(json_encode(["failure","Important info not included in the request, if you accessed this through the bot, please contact Random."]));
+	die(195);
 }
 ?>
